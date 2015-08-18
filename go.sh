2e1cb94f5e16dd7f1aca4a53356c5bc9b224e8bf
Li Wang <liwang@pivotal.io>
2015-08-18 19:50:21 -0400

    first commit

---
+++ go.sh
@@ -0,0 +1,3 @@
+gradle clean build
+cp build/libs/FFFTP-1.1.0.RELEASE.jar /Users/lwang/Documents/spring-xd-1.2.1.RELEASE/xd/custom-modules/source/FFFTP.jar
+cp build/libs/FFFTP-1.1.0.RELEASE.jar /Users/lwang/Documents/spring-xd-1.2.1.RELEASE/xd/custom-modules/source/FFFTP.jar.md5
